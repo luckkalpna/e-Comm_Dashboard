55125581fcd8ce9f88cae88558bebb6b717754b7
luckkalpna <luckkalpna@gmail.com>
2025-06-24 13:10:26 +0530

    Connect mongoDB with project

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app  = express();
 
-app.get('/', (req, res) =>{
-  res.send("App is working....")
-});
+const connnectDB = async () =>{
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("product", productSchema);
+  const data = await product.find();
+  console.log(data);
+}
+connnectDB();
 
 app.listen(5000)
