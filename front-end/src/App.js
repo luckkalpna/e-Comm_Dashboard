36c71748ae59ab8e5fbb61cd3acd86da5f6b8d99
luckkalpna <luckkalpna@gmail.com>
2025-06-24 11:05:37 +0530

    Create navbar and add routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>E-Dashboard</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Add Product Component</h1>} />
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Page</h1>} />
+          <Route path="/profile" element={<h1>Profile Page</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
