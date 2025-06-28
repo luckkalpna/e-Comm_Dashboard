6a0ad9f24d5506cdd5138dad4070e51087baf43c
luckkalpna <luckkalpna@gmail.com>
2025-06-28 17:27:00 +0530

    Set error on Add-Product form

--- front-end/src/components/AddProduct.js
+++ front-end/src/components/AddProduct.js
@@ -1,79 +1,90 @@
-import React from 'react'
+import React from "react";
 
 export default function AddProduct() {
-  const [name, setName] = React.useState('');
-  const [price, setPrice] = React.useState('');
-  const [category, setCategory] = React.useState('');
-  const [company, setCompany] = React.useState('');
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const [error, setError] = React.useState(false);
 
-  const addProduct = async () =>{
-    console.log(name, price, category, company);
+  const addProduct = async () => {
+    // console.log(name, price, category, company);
+    // console.log(!name)
+    if (!name || !price || !category || !company) {
+      setError(true);
+      return false;
+    }
     const userId = JSON.parse(localStorage.getItem("user"))._id;
     console.log(userId);
-      let result = await fetch("http://localhost:5000/add-product", {
+    let result = await fetch("http://localhost:5000/add-product", {
       method: "POST",
-      body: JSON.stringify({name, price, category, userId, company}),
+      body: JSON.stringify({ name, price, category, userId, company }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
     });
     result = await result.json();
     localStorage.setItem("product", JSON.stringify(result));
     console.log(result);
-    // if(result){
-    //   Navigate('/');
-    // }
-  }
+  };
   return (
     <>
-    <div className="container my-4">
-      <h3>Add Your Product</h3>
-      <form>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Enter product name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="price"
-            placeholder="Enter price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="category"
-            placeholder="Enter category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="company"
-            placeholder="Enter company name"
-            value={company}
-            onChange={(e) => setCompany(e.target.value)}
-          />
-        </div>
-        <button onClick={addProduct} type="button" className="btn btn-primary">
-          Add Product
-        </button>
+      <div className="container my-4">
+        <h3>Add Your Product</h3>
+        <form>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Enter product name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {error && !name && <span className="error-msg">Enter valid name</span>}
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="price"
+              placeholder="Enter price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            {error && !price && <span className="error-msg">Enter valid name</span>}
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="category"
+              placeholder="Enter category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            {error && !category && <span className="error-msg">Enter valid name</span>}
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="company"
+              placeholder="Enter company name"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+            />
+            {error && !company && <span className="error-msg">Enter valid name</span>}
+          </div>
+          <button
+            onClick={addProduct}
+            type="button"
+            className="btn btn-primary"
+          >
+            Add Product
+          </button>
         </form>
-        </div>
+      </div>
     </>
-  )
+  );
 }
