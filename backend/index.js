bd1e97918ecc9bf8883815bd4c7cec52091f22f9
luckkalpna <luckkalpna@gmail.com>
2025-06-28 12:02:03 +0530

    Create login API

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-require('./db/config');
-const User = require('./db/User');
-const cors = require('cors');
-const app  = express();
+const express = require("express");
+require("./db/config");
+const User = require("./db/User");
+const cors = require("cors");
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', async (req, res) =>{
+app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password; // Exclide password from the response
   res.send(result);
 });
 
-app.listen(5000)
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password"); //Exclude password from the response
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No User Found" });
+    }
+  } else {
+    res.send({ result: "No User Found" });
+  }
+});
+
+app.listen(5000);
