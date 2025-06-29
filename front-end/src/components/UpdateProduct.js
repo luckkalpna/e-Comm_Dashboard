35c590086de1d1d4dab437e34d5eb7df4f7a6d90
luckkalpna <luckkalpna@gmail.com>
2025-06-29 14:16:07 +0530

    Integrate API for autofill product details

--- front-end/src/components/UpdateProduct.js
+++ front-end/src/components/UpdateProduct.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 
 export default function UpdateProduct() {
 const [name, setName] = React.useState("");
   const [price, setPrice] = React.useState("");
   const [category, setCategory] = React.useState("");
   const [company, setCompany] = React.useState("");
+  const params = useParams();
+
+  useEffect(() =>{
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () =>{
+    console.log(params);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`);
+    result = await result.json();
+    setName(result.name);
+    setCategory(result.category);
+    setPrice(result.price);
+    setCompany(result.company);
+  }
 
   const updateProduct = async () => {
     console.log(name, price, category, company);
