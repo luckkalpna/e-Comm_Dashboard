e69016d263fa9c46ca77834268f6d9cf2fec4266
luckkalpna <luckkalpna@gmail.com>
2025-06-29 17:05:39 +0530

    Integrate Search API

--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -14,39 +14,83 @@ export default function ProductList() {
     setProducts(result);
   };
   // console.log("products", products); // ✅ Log here
-  const deleteProduct = async (id) =>{
-    let result = await fetch (`http://localhost:5000/product/${id}`, {
-      method: "DELETE"
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "DELETE",
     });
     result = await result.json();
-    if(result){
+    if (result) {
       getProducts();
     }
     // console.log(id)
-  }
+  };
+
+  const searchHandler = async (event) => {
+    // console.log(event.target.value);
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
 
   return (
     <>
       <div className="product-list">
         <h3>Our Product List</h3>
+        <div className="container">
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Product"
+              onChange={searchHandler}
+            />
+          </div>
+        </div>
         <ul>
           <li>S. No.</li>
           <li>Name</li>
           <li>Price</li>
           <li>Category</li>
+          <li>Company</li>
           <li>Operation</li>
         </ul>
-        {products.map((item, index) => (
-          <ul className="product-item" key={item._id}>
-            <li>{index + 1}</li>
-            <li>{item.name}</li>
-            <li>$ {item.price}</li>
-            <li>{item.category}</li>
-            <li><button type="button" className="btn btn-danger btn-sm" onClick={()=>{deleteProduct(item._id)}}>Delete</button> / 
-            <Link to={"/update/"+item._id}> <button type="button" className="btn btn-info btn-sm">Update</button></Link>
-            </li>
-          </ul>
-        ))}
+        {
+          products.length>0 ? products.map((item, index) => (
+            <ul className="product-item" key={item._id}>
+              <li>{index + 1}</li>
+              <li>{item.name}</li>
+              <li>$ {item.price}</li>
+              <li>{item.category}</li>
+              <li>{item.company}</li>
+              <li>
+                <button
+                  type="button"
+                  className="btn btn-danger btn-sm"
+                  onClick={() => {
+                    deleteProduct(item._id);
+                  }}
+                >
+                  Delete
+                </button>{" "}
+                /
+                <Link to={"/update/" + item._id}>
+                  {" "}
+                  <button type="button" className="btn btn-info btn-sm">
+                    Update
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          )) : 
+          (<h3>No Result Found</h3>)
+        }
       </div>
     </>
   );
