367d184837061ffa13e43e60f0422a07b3af2f89
luckkalpna <luckkalpna@gmail.com>
2025-06-29 12:01:46 +0530

    List all product on Product Page

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,19 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post("/add-product" , async (req, res) =>{
+app.post("/add-product", async (req, res) => {
   let product = new Product(req.body);
   let result = await product.save();
   res.send(result);
-})
+});
+
+app.get("/products", async (req, res) => {
+  let products = await Product.find();
+  if (products.length > 0) {
+    res.send(products);
+  } else {
+    res.send({ result: "No Products Found" });
+  }
+});
 
 app.listen(5000);
